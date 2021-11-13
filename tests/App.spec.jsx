d0fba34d63c4306f033abbb2659133f754de79bb
tomwilson <tomas.f.wilson@gmail.com>
2021-11-13 10:51:57 -0300

    test: create basic test to ensure configuration works

--- tests/App.spec.jsx
+++ tests/App.spec.jsx
@@ -1,8 +1,13 @@
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
-import Enzyme from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import React from 'react';
+import App from '../src/components/App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-describe('', () => {
-  test('should render without error', () => {});
+describe('Main react component', () => {
+  test('should render  non-empty and without error a', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
 });
