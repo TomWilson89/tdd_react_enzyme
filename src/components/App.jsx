e0cf4b46b22ecd82c80168cb4a82581c277775be
tomwilson <tomas.f.wilson@gmail.com>
2021-11-14 22:16:09 -0300

    feat: add congrats and guessedWords components into the home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { GuessedWords } from '.';
 import '../styles/global.css';
+import Congrats from './congrats';
 
 const App = () => {
+  const guessedWords = [
+    { guessedWord: 'train', letterMatchCount: 3 },
+    { guessedWord: 'agile', letterMatchCount: 1 },
+    { guessedWord: 'party', letterMatchCount: 5 },
+  ];
   return (
-    <div className="App">
-      <h1> Hello, World! </h1>
+    <div className="container">
+      <h1>Jotto</h1>
+      <Congrats success />
+      <GuessedWords guessedWords={guessedWords} />
     </div>
   );
 };
