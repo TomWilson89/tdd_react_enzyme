0762bf2f4507ed073a189fc5a4e2a638389e2b99
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-15 11:01:33 -0300

    feat: add input component and ensure it renders without error

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Congrats } from './congrats';
 export { default as GuessedWords } from './guessedWords';
+export { default as Input } from './input';
---
+++ src/components/input/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Input = () => {
+  return <div data-testid="component-input" />;
+};
+
+export default Input;
