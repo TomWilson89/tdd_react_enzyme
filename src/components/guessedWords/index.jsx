76efa5d56049f5708c9d527873d105fcef560cc2
tomwilson <tomas.f.wilson@gmail.com>
2021-11-14 21:16:32 -0300

    feat: ensure GuessedWords component renders without error

---
+++ src/components/guessedWords/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const GuessedWords = () => {
+  return <div data-testid="component-guessed-words" />;
+};
+
+GuessedWords.propTypes = {};
+export default GuessedWords;
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as Congrats } from './congrats';
+export { default as GuessedWords } from './guessedWords';
