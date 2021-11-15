97e2690abf822e51323b0544b5de97aa84b7beb3
tomwilson <tomas.f.wilson@gmail.com>
2021-11-14 21:26:17 -0300

    feat: ensure not throws warning if expected props are passed

--- src/components/guessedWords/index.jsx
+++ src/components/guessedWords/index.jsx
@@ -1,8 +1,46 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-const GuessedWords = () => {
-  return <div data-testid="component-guessed-words" />;
+const GuessedWords = ({ guessedWords }) => {
+  return (
+    <div data-testid="component-guessed-words">
+      {guessedWords.length ? (
+        <div data-testid="guessed-words">
+          <h3>Guessed Words</h3>
+          <table className="table table-sm">
+            <thead className="thead-light">
+              <tr>
+                <th>#</th>
+                <th>Guess</th>
+                <th>Matching Letters</th>
+              </tr>
+            </thead>
+            <tbody>
+              {guessedWords.map((word, index) => {
+                return (
+                  <tr key={word}>
+                    <td>{index + 1}</td>
+                    <td>{word.guessedWord}</td>
+                    <td>{word.letterMatchCount}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <span data-testid="guessed-words-intro">Guess the secret word!</span>
+      )}
+    </div>
+  );
 };
 
-GuessedWords.propTypes = {};
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 export default GuessedWords;
