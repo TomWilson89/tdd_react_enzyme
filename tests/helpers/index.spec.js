5078c7f06056d7c9a2da13ce2b6053f84adb3ddb
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-15 23:51:22 -0300

    test: ensure getLetterMAtchCount returns 0 if there are no matching letters

---
+++ tests/helpers/index.spec.js
@@ -0,0 +1,14 @@
+import { getLetterMatchCount } from '../../src/helpers';
+
+describe('getLetterMatchCount', () => {
+  const secretWord = 'party';
+  test('should return 0 when there are no matching letters', () => {
+    const testWord = 'bones';
+    const matchCount = getLetterMatchCount(testWord, secretWord);
+    expect(matchCount).toBe(0);
+  });
+
+  test('should return 3 when there are 3 matching letters', () => {});
+
+  test('should return correct count when there are duplicate letters in the guess', () => {});
+});
