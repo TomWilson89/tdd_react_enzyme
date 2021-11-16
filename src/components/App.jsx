35abd2eab8c2aa010ecd4415eb3acf9606b24c24
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-16 15:28:13 -0300

    feat: add input component to app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { GuessedWords } from '.';
+import { GuessedWords, Input } from '.';
+import { getLetterMatchCount } from '../helpers';
 import '../styles/global.css';
 import Congrats from './congrats';
 
 const App = () => {
-  const guessedWords = [
-    { guessedWord: 'train', letterMatchCount: 3 },
-    { guessedWord: 'agile', letterMatchCount: 1 },
-    { guessedWord: 'party', letterMatchCount: 5 },
-  ];
+  const guessedWords = [];
+
+  const success = false;
+  const secretWord = 'party';
+
+  const handleSubmit = (values) => {
+    console.log(values);
+    const letterMatchCount = getLetterMatchCount(secretWord, values);
+    const result = {
+      guessedWord: values,
+      letterMatchCount,
+    };
+
+    guessedWords.push(result);
+  };
   return (
     <div className="container" data-testid="component-app">
       <h1>Jotto</h1>
-      <Congrats success />
+      <Input secretWord={secretWord} success={success} handleSubmit={handleSubmit} />
+      <Congrats success={success} />
       <GuessedWords guessedWords={guessedWords} />
     </div>
   );
