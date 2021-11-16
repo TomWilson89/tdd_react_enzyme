4737fe86c73dec09bff596361fcbb25720d06f0f
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-15 22:48:27 -0300

    feat: ensure input component state updates with correct values

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -3,7 +3,32 @@ import React from 'react';
 
 // eslint-disable-next-line no-unused-vars
 const Input = ({ secretWord }) => {
-  return <div data-testid="component-input" />;
+  const [state, setState] = React.useState({
+    currentGuess: '',
+  });
+
+  const handleChange = (e) => {
+    setState({
+      currentGuess: e.target.value,
+    });
+  };
+  return (
+    <div data-testid="component-input">
+      <form className="form-inline">
+        <input
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+          data-testid="input-box"
+          value={state.currentGuess}
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn btn-primary mb-2" data-testid="submit-button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 Input.propTypes = {
