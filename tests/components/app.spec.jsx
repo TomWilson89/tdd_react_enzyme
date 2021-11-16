45fa0f2a3bda972b4ef5477b51fcd64b31b73b03
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-16 14:13:03 -0300

    test: ensure App component renders withour error

---
+++ tests/components/app.spec.jsx
@@ -0,0 +1,18 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import App from '../../src/components/App';
+import { findByTestAttribute } from '../utils';
+
+const makeSut = () => {
+  const sut = shallow(<App />);
+
+  return { sut };
+};
+
+describe('App component', () => {
+  test('should render withour any errors', () => {
+    const { sut } = makeSut();
+    const appComponent = findByTestAttribute(sut, 'component-app');
+    expect(appComponent.length).toBe(1);
+  });
+});
