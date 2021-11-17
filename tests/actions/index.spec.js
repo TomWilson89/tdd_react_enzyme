8e76513bc36c2c0ccf5b21dae609f7445f3e3316
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-16 22:42:31 -0300

    test: ensure getSecretWord returns a secret word from server

---
+++ tests/actions/index.spec.js
@@ -0,0 +1,29 @@
+import 'core-js/stable';
+import moxios from 'moxios';
+import 'regenerator-runtime/runtime';
+import { getSecretWord } from '../../src/actions';
+
+describe('getSecretWord', () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  test('should return secret word', async () => {
+    const secretWord = 'party';
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: secretWord,
+      });
+    });
+
+    // update to test app in Redux / context sections
+    const response = await getSecretWord();
+    expect(response).toBe(secretWord);
+  });
+});
