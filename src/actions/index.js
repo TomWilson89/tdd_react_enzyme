f17732affa49efc27c473a570c9f844ea8635f79
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-17 07:48:23 -0300

    feat: ensure correctGuess returns correct type

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
+import { SUCCESS_TYPES } from '../types';
 
 export const getSecretWord = async () => {
   const { data } = await axios.get('http://localhost:3030');
   return data;
+};
+
+export const correctGuess = () => {
+  return {
+    type: SUCCESS_TYPES.SUCCESS,
+  };
 };
