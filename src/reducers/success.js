0d4f3380eb160b20598fe66062a68eb33ca497fe
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-17 07:44:19 -0300

    refactor: move types to its own folder

--- src/reducers/success.js
+++ src/reducers/success.js
@@ -1,6 +1,8 @@
+import { SUCCESS_TYPES } from '../types/index';
+
 export default (state = false, action) => {
   switch (action.type) {
-    case 'SUCCESS':
+    case SUCCESS_TYPES.SUCCESS:
       return action.payload;
     default:
       return state;
--- src/store/types.js
+++
@@ -1,7 +0,0 @@
-const SUCCESS_TYPES = {
-  SUCCESS: 'SUCCESS',
-};
-
-export default {
-  SUCCESS_TYPES,
-};
