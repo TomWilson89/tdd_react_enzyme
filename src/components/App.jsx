09727e7794a8469b2930c74927055f0116c40a2a
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-18 14:16:46 -0300

    feat: add language context to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react';
 import { GuessedWords, Input } from '.';
 import { getSecretWord } from '../actions';
-import { GUESS_WORDS } from '../constants/types';
+import { GUESS_WORDS, LANGUAGES } from '../constants/types';
+import languageContext from '../context/language';
 import '../styles/global.css';
 import Congrats from './congrats';
+import LanguagePicker from './languagePicker';
 
 const initialState = {
   success: false,
   secretWord: null,
   guessedWords: [],
+  language: 'en',
 };
 
 const reducer = (state = initialState, action) => {
@@ -20,6 +23,13 @@ const reducer = (state = initialState, action) => {
       };
     }
 
+    case LANGUAGES.SET_LANGUAGE: {
+      return {
+        ...state,
+        language: action.payload,
+      };
+    }
+
     default:
       throw new Error(`Action type: ${action.type} not supported`);
   }
@@ -28,9 +38,13 @@ const reducer = (state = initialState, action) => {
 const App = () => {
   const [state, dispatch] = React.useReducer(reducer, initialState);
 
-  const setSecretWord = (secretWord) => [
-    dispatch({ type: GUESS_WORDS.SET_SECRET_WORD, payload: secretWord }),
-  ];
+  const setSecretWord = (secretWord) => {
+    dispatch({ type: GUESS_WORDS.SET_SECRET_WORD, payload: secretWord });
+  };
+
+  const setLanguage = (language) => {
+    dispatch({ type: LANGUAGES.SET_LANGUAGE, payload: language });
+  };
 
   useEffect(() => {
     getSecretWord(setSecretWord);
@@ -46,12 +60,16 @@ const App = () => {
     );
   }
   return (
-    <div className="container" data-testid="component-app">
-      <h1>Jotto</h1>
-      <Input secretWord={state.secretWord} success={state.success} />
-      <Congrats success={state.success} />
-      <GuessedWords guessedWords={state.guessedWords} />
-    </div>
+    <languageContext.Provider value={state.language}>
+      <LanguagePicker setLanguage={setLanguage} />
+
+      <div className="container" data-testid="component-app">
+        <h1>Jotto</h1>
+        <Input secretWord={state.secretWord} success={state.success} />
+        <Congrats success={state.success} />
+        <GuessedWords guessedWords={state.guessedWords} />
+      </div>
+    </languageContext.Provider>
   );
 };
 
